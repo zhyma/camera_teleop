0353f4ed1f838d2a2211ef0a688d7e470b8a5ea1
Zhaoyuan Ma <zma3@wpi.edu>
2019-12-17 16:34:37 -0500

    just made it workable

--- src/subsribe_headset.py
+++ src/subsribe_headset.py
@@ -4,23 +4,28 @@ import rospy
 import serial
 from camera_teleop.msg import HeadMotion
 
+def int2byte(i):
+    if i >= 0:
+        return i
+    else:
+        return 256+i
+
 def callback(data):
-   rospy.loginfo("StartBit: %s, Angle1: %d, Speed1: %d, Angle2: %d, Speed2: %d, Stop Bit: %s" % (data.startBit,data.angle1,data.speed1,data.angle2,data.speed2,data.stopBit))
-   pass
-   com = serial.Serial('/dev/ttyACM0', 57600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
-   pass
-   values = bytearray([data.startBit, data.angle1, data.speed1, data.angle2, data.speed2, data.stopBit])
-   # print values
-   com.write(values)
-   data = com.read(2)
-   print data
+    rospy.loginfo("StartBit: %s, Angle1: %d, Speed1: %d, Angle2: %d, Speed2: %d, Stop Bit: %s" % (data.startBit,data.angle1,data.speed1,data.angle2,data.speed2,data.stopBit))
+    com = serial.Serial('/dev/ttyACM0', 57600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
+    values = bytearray([0x80, int2byte(data.angle1), data.speed1, int2byte(data.angle2), data.speed2, 0x7f])
+    print [hex(i) for i in values]
+    # print values
+    com.write(values)
+    data = com.read(2)
+    print data
 
 def listener():
-   rospy.init_node('subscribe_headset_data', anonymous=True)
-   rospy.Subscriber("publish_headset_data", HeadMotion, callback)
+    rospy.init_node('subscribe_headset_data', anonymous=True)
+    rospy.Subscriber("publish_headset_data", HeadMotion, callback)
 
-   rospy.spin()
+    rospy.spin()
 
 if __name__ == '__main__':
-   listener()
+    listener()
 

40711ed7c2a422a467fe8e210c9ccb9c7a206095
Zhaoyuan Ma <zma3@wpi.edu>
2019-12-18 18:14:37 -0500

    Connected to Unity, workable prototype

--- src/publish_headset_data.py
+++ src/publish_headset_data.py
@@ -2,21 +2,41 @@
 
 import rospy
 from std_msgs.msg import String
+from camera_teleop.msg import HeadMotion
+import socket
 
-RATE = 10
+RATE = 20
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+def get_angle(i):
+    i = int(float(i))
+    if i > 180:
+        i = i-360
+    if i < -180:
+        i = 360+i
+    return i
+
+def vive_status_pub():
+    pub = rospy.Publisher('publish_headset_data', HeadMotion, queue_size=10)
+    rospy.init_node('vive_status', anonymous=True)
     rate = rospy.Rate(RATE)
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        buffer, addr = sock.recvfrom(2048)
+        buffer = buffer.split(',')
+        rospy.loginfo(buffer)
+        head_motion = HeadMotion()
+        head_motion.angle1 = get_angle(buffer[4])
+        head_motion.speed1 = 30
+        head_motion.angle2 = get_angle(buffer[6])
+        head_motion.speed2 = 30
+        rospy.loginfo(head_motion)
+        pub.publish(head_motion)
         rate.sleep()
 
 if __name__ == '__main__':
+    address = ("130.215.206.220", 8001)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto('request', address)
     try:
-        talker()
+        vive_status_pub()
     except rospy.ROSInterruptException:
         pass
--- src/subsribe_headset.py
+++ src/subsribe_headset.py
@@ -4,25 +4,31 @@ import rospy
 import serial
 from camera_teleop.msg import HeadMotion
 
+com = serial.Serial('/dev/ttyACM0', 57600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
+
 def int2byte(i):
+    if i > 180:
+        i = i-360
+    if i < -180:
+        i = 360+i
     if i >= 0:
         return i
     else:
         return 256+i
 
 def callback(data):
-    rospy.loginfo("StartBit: %s, Angle1: %d, Speed1: %d, Angle2: %d, Speed2: %d, Stop Bit: %s" % (data.startBit,data.angle1,data.speed1,data.angle2,data.speed2,data.stopBit))
-    com = serial.Serial('/dev/ttyACM0', 57600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
+    rospy.loginfo("Angle1: %d, Speed1: %d, Angle2: %d, Speed2: %d" % (data.angle1,data.speed1,data.angle2,data.speed2))
+    
     values = bytearray([0x80, int2byte(data.angle1), data.speed1, int2byte(data.angle2), data.speed2, 0x7f])
     print [hex(i) for i in values]
     # print values
     com.write(values)
-    data = com.read(2)
-    print data
+    #data = com.read(2)
+    #print data
 
 def listener():
     rospy.init_node('subscribe_headset_data', anonymous=True)
-    rospy.Subscriber("publish_headset_data", HeadMotion, callback)
+    rospy.Subscriber('publish_headset_data', HeadMotion, callback)
 
     rospy.spin()
 

a73cc8717dc1be0c7648f630d0ffb4163da04376
Zhaoyuan Ma <zma3@wpi.edu>
2019-12-20 11:07:53 -0500

    Add two controllers' position as well

--- src/publish_headset_data.py
+++ src/publish_headset_data.py
@@ -9,37 +9,43 @@ from math import pi
 
 RATE = 20
 
-def get_angle(i):
-    i = int(float(i))
-    if i > 180:
-        i = i-360
-    if i < -180:
-        i = 360+i
-    return i
-
+# degree to radians
 def d2r(i):
     return float(i) * pi / 180.0
 
+def fillin_pos(data):
+    qt = tf.transformations.quaternion_from_euler(d2r(data[3]), d2r(data[4]), d2r(data[5]))
+    pose = PoseStamped()
+    pose.header.stamp = rospy.Time.now()
+    pose.header.frame_id = "world"
+    pose.pose.position.x = float(data[0])
+    pose.pose.position.y = float(data[1])
+    pose.pose.position.z = float(data[2])
+    pose.pose.orientation.x = qt[0]
+    pose.pose.orientation.y = qt[1]
+    pose.pose.orientation.z = qt[2]
+    pose.pose.orientation.w = qt[3]
+
+    return pose
+
 def vive_status_pub():
-    pub_headquat = rospy.Publisher('/vive/twist5', PoseStamped, queue_size=10)
+    pub_right = rospy.Publisher('/vive/twist3', PoseStamped, queue_size=10)
+    pub_left  = rospy.Publisher('/vive/twist4', PoseStamped, queue_size=10)
+    pub_head  = rospy.Publisher('/vive/twist5', PoseStamped, queue_size=10)
+
     rospy.init_node('vive_status', anonymous=True)
     rate = rospy.Rate(RATE)
     while not rospy.is_shutdown():
         buffer, addr = sock.recvfrom(2048)
         buffer = buffer.split(',')
 
-        head_pos = PoseStamped()
-        qt = tf.transformations.quaternion_from_euler(d2r(buffer[4]), d2r(buffer[5]), d2r(buffer[6]))
-        head_pos.header.stamp = rospy.Time.now()
-        head_pos.header.frame_id = "map"
-        head_pos.pose.position.x = float(buffer[1])
-        head_pos.pose.position.y = float(buffer[2])
-        head_pos.pose.position.z = float(buffer[3])
-        head_pos.pose.orientation.x = qt[0]
-        head_pos.pose.orientation.y = qt[1]
-        head_pos.pose.orientation.z = qt[2]
-        head_pos.pose.orientation.w = qt[3]
-        pub_headquat.publish(head_pos)
+        head_pos = fillin_pos(buffer[1:7])
+        right_pos = fillin_pos(buffer[8:14])
+        left_pos = fillin_pos(buffer[15:26])
+        
+        pub_right.publish(right_pos)
+        pub_left.publish(left_pos)
+        pub_head.publish(head_pos)
 
         rate.sleep()
 

818adcc6288d8f244f0d2aad643770d0f94128ec
Zhaoyuan Ma <zma3@wpi.edu>
2020-01-14 13:51:36 -0500

    Removed unnecessary topics. Changed port to 23023. Connect/reconnect states.

--- src/publish_headset_data.py
+++ src/publish_headset_data.py
@@ -53,19 +53,22 @@ def fillin_joy(data, ctrl_name):
 
 
 def vive_status_pub():
+    # connect state and reconnect counter
+    con_state = False
+    con_cnt = 0
     # Don't know what is the /vive/twist3 or 4 is for.
-    pub_twist_r = rospy.Publisher('/vive/twist3', PoseStamped, queue_size=10)
-    pub_twist_l = rospy.Publisher('/vive/twist4', PoseStamped, queue_size=10)
+    pub_twist_r = rospy.Publisher('/vive/twist3', PoseStamped, queue_size=1)
+    pub_twist_l = rospy.Publisher('/vive/twist4', PoseStamped, queue_size=1)
 
     # This one is send to control the head camera
-    pub_head    = rospy.Publisher('/vive/twist5', PoseStamped, queue_size=10)
+    pub_head    = rospy.Publisher('/vive/twist5', PoseStamped, queue_size=1)
     
     # Those are send to control the end effectors
-    pub_pos_r = rospy.Publisher('/Right_Hand',TransformStamped, queue_size=10)
-    pub_pos_l = rospy.Publisher('/Left_Hand', TransformStamped, queue_size=10)
+    pub_pos_r = rospy.Publisher('/Right_Hand',TransformStamped, queue_size=1)
+    pub_pos_l = rospy.Publisher('/Left_Hand', TransformStamped, queue_size=1)
 
-    pub_key_r = rospy.Publisher('/vive/controller_LHR_FFFB7FC3/joy', Joy, queue_size=10)
-    pub_key_l = rospy.Publisher('/vive/controller_LHR_FF7FBBC0/joy', Joy, queue_size=10)
+    pub_key_l = rospy.Publisher('/vive/controller_LHR_FFFB7FC3/joy', Joy, queue_size=1)
+    pub_key_r = rospy.Publisher('/vive/controller_LHR_FF7FBBC0/joy', Joy, queue_size=1)
 
     rospy.init_node('vive_status', anonymous=True)
     rate = rospy.Rate(RATE)
@@ -73,19 +76,23 @@ def vive_status_pub():
         try:
        	    sock.settimeout(1)
             buffer, addr = sock.recvfrom(2048)
+            if not con_state:
+                print "connected"
+                con_state = True
             buffer = buffer.split(',')
-            print buffer
+            #print buffer
 
             head_pos, _= fillin_pos(buffer[1:7], [0, 0, 0])
             right_pos, msg_r = fillin_pos(buffer[8:14], [-pi/2, pi, 0])
             left_pos, msg_l  = fillin_pos(buffer[15:21], [pi/2, pi, 0])
 
-            joy_r = fillin_joy(buffer[22:29], 'controller_LHR_FFFB7FC3')
-            joy_l = fillin_joy(buffer[30:37], 'controller_LHR_FF7FBBC0')
+            
+            joy_r = fillin_joy(buffer[22:29], 'controller_LHR_FF7FBBC0')
+            joy_l = fillin_joy(buffer[30:37], 'controller_LHR_FFFB7FC3')
 
-            pub_twist_r.publish(right_pos)
+            #pub_twist_r.publish(right_pos)
             pub_pos_r.publish(msg_r)
-            pub_twist_l.publish(left_pos)
+            #pub_twist_l.publish(left_pos)
             pub_pos_l.publish(msg_l)
             pub_head.publish(head_pos)
             pub_key_r.publish(joy_r)
@@ -93,12 +100,14 @@ def vive_status_pub():
 
             rate.sleep()
         except:
-            print 'no server detected, reconnecting'
-            sock.sendto('request', address)
+            print 'no server detected, reconnecting ' + str(con_cnt)
+            con_state = False
+            con_cnt += 1
             rate.sleep()
+            sock.sendto('request', address)
 
 if __name__ == '__main__':
-    address = ("130.215.206.220", 8001)
+    address = ("130.215.206.220", 23023)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto('request', address)
     try:
